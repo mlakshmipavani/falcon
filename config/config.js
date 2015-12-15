bd20ac262e6765343ceb99762bfb5e1e5aa5522b
Jaydeep <jaydp17@gmail.com>
2015-12-15 12:18:25 +0530

    copy cat bot ready with error checking

--- config/config.js
+++ config/config.js
@@ -19,6 +19,8 @@ config.bunyan.name = config.appName;
 //noinspection JSUnresolvedVariable
 config.bunyan.level = process.env.BUNYAN_LEVEL || 'debug';
 
+config.botServerUrl = process.env.BOT_SERVER_URL || 'http://localhost:5000/';
+
 /**
  * Export
  */
--- routes/bot-msg-routes.js
+++ routes/bot-msg-routes.js
@@ -2,6 +2,8 @@
 
 var request = require('request-promise');
 var apiVersion = require('../config/api-version.js');
+var ErrorController = require('../controllers/error-controller.js');
+var config = require('../config/config.js');
 
 /**
  * A route that forwards a message to the respective bot
@@ -12,15 +14,25 @@ class BotMsgRoutes {
 
     app.post({path: '/msg/:botHandle', version: apiVersion.v1}, (req, res) => {
 
-      // get the handle
-      var handle = req.params.botHandle;
+      // error checking
+      req.assert('botHandle', 'botHandle is a required param').notEmpty();
+      req.assert('body', 'body is a required param').notEmpty();
+      var errors = req.validationErrors();
+      if (errors) {
+        return ErrorController.paramError(req, res, errors);
+      }
 
-      // remove the handle from params
-      delete req.params.botHandle;
-      var requestOptions = {url: `http://localhost:5000/${handle}`, form: req.params, json: true};
+      //noinspection JSUnresolvedVariable
+      var botHandle = req.params.botHandle;
+      var body = req.params.body;
+
+      var requestOptions = {url: `${config.botServerUrl}${botHandle}`, form: {body}, json: true};
 
       // send the response back to the client
-      request.post(requestOptions).then(response => res.send(response));
+      request.post(requestOptions).then(response => {
+        var fullResponse = {_id: Math.random().toString(36).substring(7), body: response, type: 0};
+        res.json(fullResponse);
+      });
     });
   }
 }
