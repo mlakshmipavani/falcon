261b3059b5988d4965423f32763090af8e4f7969
parth <parthpatolia@gmail.com>
2016-03-07 12:49:36 +0530

    test for different response from chkpnrapi

--- test/unit/controllers/pnr-controllers/chk-pnr-sts-irctc-api.js
+++ test/unit/controllers/pnr-controllers/chk-pnr-sts-irctc-api.js
@@ -30,10 +30,36 @@ describe('ChkPnrStsApi', () => {
     chartStatus: 'CHART NOT PREPARED'
   };
 
+  const expectedRes2 = {
+    trainNumber: '12321',
+    trainName: 'HWH MUMBAI MAIL',
+    boardingDate: '4- 3-2016',
+    from: 'JBP',
+    to: 'NK',
+    reservedUpto: 'NK',
+    boardingPoint: 'JBP',
+    class: 'SL',
+    passengers: [
+      {
+        name: '1',
+        bookingStatus: 'W/L   11,HO',
+        currentStatus: 'S7  , 25'
+      }
+    ],
+    bookingFare: '435',
+    chartStatus: 'CHART PREPARED'
+  };
+
   it('should get the pnr status details out of html', () => {
     //noinspection JSAccessibilityCheck,JSUnresolvedVariable
     return ChkPnrStsApi._extractPnrDetail(apiResponse).should.deep.equal(expectedResponse);
   });
+
+  it('should correctly parse the different response from api', () => {
+    //noinspection JSAccessibilityCheck,JSUnresolvedVariable
+    return ChkPnrStsApi._extractPnrDetail(apiRes2).should.deep.equal(expectedRes2);
+  });
+
 });
 
 const apiResponse = `
@@ -82,6 +108,64 @@ color: rgba(215, 0, 0, 0.87);
 <h3>Charting Status: </h3>
 <br/>
 <thead><th>No.</th><th>Booking Status (Coach No , Berth No., Quota)</th><th>Current Status</th></thead><tr><td>Passenger 1</td><td>W/L    2,GNWL  </td><td>RAC    8</td></tr><tr><td>Passenger 2</td><td>W/L    3,GNWL  </td><td>RAC    9</td></tr><tr><td>Total Booking Fare</td><td>1700</td><td></td></tr><tr><td>Charting Status</td><td> CHART NOT PREPARED </td><td></td></tr></table>
+</br>PNR Status Details by <a href='http://CheckPNRstatusIRCTC.in/'>http://CheckPNRstatusIRCTC.in/</a></br></br><div id="example5" class='non-printable'>
+<div id="shareme" data-url="http://checkpnrstatusirctc.in/" data-text="Check PNR Status of your train tickets, Train Schedule, Cancelled Trains, Diverted Trains, etc."></div>
+</div>
+<script>$("#shareme").sharrre({share:{googlePlus:true,facebook:true,twitter:true,digg:true,delicious:true,linkedin:true,},buttons:{googlePlus:{size:"tall",annotation:"bubble"},facebook:{layout:"box_count"},twitter:{count:"vertical"},digg:{type:"DiggMedium"},delicious:{size:"tall"},linkedin:{counter:"top"},pinterest:{media:"http://4.bp.blogspot.com/-k4A9Yzvw4Q0/U8p7od-GCuI/AAAAAAAAFqo/ycG9S8lVaUY/s1600/Indian-Railways-PNR-status-Enquiry.jpg",description:$("#shareme").data("text"),layout:"vertical"}},enableHover:false,enableCounter:false,enableTracking:true});</script>
+<style type="text/css">#example5{margin: 0 auto;max-width: 400px; overflow: hidden;}.sharrre .button{float:left;width:60px;}.sharrre .linkedin{width:70px}</style>
+`;
+
+
+const apiRes2 = `
+
+
+
+
+
+
+<table width="100%" border="0" cellpadding="0" cellspacing="1" class="table_border" style="font-size: 15px;">
+<style>
+.heading_table_top {
+font-size:20px !important;
+padding-top: 20px;
+padding-bottom: 20px;
+color: rgba(215, 0, 0, 0.87);
+}
+</style>
+<div class='non-printable btn btn-primary btn-large' onclick='window.print();'>Print</div><style>.heading_table_top{font-size:14px;}</style><h5><b>Train Number</b> : <a href="/train/12321">12321 <span class='non-printable'>(Check Train Details)</span></a></h5>
+<h5><b>Train Name</b> : HWH MUMBAI MAIL</h5>
+<h5><b>Boarding Date</b> : 4- 3-2016</h5>
+<h5><b>From</b> :  <a href="/station/JBP">JBP</a></h5>
+<h5><b>To</b> :  <a href="/station/NK">NK</a></h5>
+<h5><b>Reserved Upto</b> :  <a href="/train/12321">NK</a></h5>
+<h5><b>Boarding Point</b> :  <a href="/train/12321">JBP</a></h5>
+<h5><b>Class</b> : SL</h5></table>
+<table width="100%" border="0" cellpadding="0" cellspacing="1" class="table_border mdl-js-data-table" id="center_table" style='font-size: 15px;'>
+<div class="non-printable">
+
+
+
+
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- CPNRLinkResp -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2530044916843200"
+     data-ad-slot="3096662173"
+     data-ad-format="link"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
+
+
+
+</div>
+<h3>Charting Status: </h3>
+<br/>
+<thead><th>No.</th><th>Booking Status (Coach No , Berth No., Quota)</th><th>Current Status</th></thead><tr><td>Passenger 1</td><td>W/L   11,HO    </td><td>S7  , 25</td></tr><tr><td>Total Booking Fare</td><td>435</td><td></td></tr><tr><td>Charting Status</td><td>   CHART PREPARED   </td><td></td></tr><tr><td>Train Status</td><td>TRAIN IS RUNNING LATE BY 03 Hour(s) and 05 Minute(s)</td><td></td></tr></table>
 </br>PNR Status Details by <a href='http://CheckPNRstatusIRCTC.in/'>http://CheckPNRstatusIRCTC.in/</a></br></br><div id="example5" class='non-printable'>
 <div id="shareme" data-url="http://checkpnrstatusirctc.in/" data-text="Check PNR Status of your train tickets, Train Schedule, Cancelled Trains, Diverted Trains, etc."></div>
 </div>
