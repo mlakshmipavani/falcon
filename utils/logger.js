7b9a49c13c28b146f89643c01d48aabcf889435c
Jaydeep <jaydp17@gmail.com>
2016-03-16 22:30:29 +0530

    Just log the logs on console, and let other container handle the pushing to loggly part
    This reverts commit 5af9e5199e86b06888dac0b7c968140512e24c27.

--- utils/logger.js
+++ utils/logger.js
@@ -2,7 +2,6 @@
 
 const bunyan = require('bunyan');
 const config = require('../config/config');
-const Bunyan2Loggly = require('bunyan-loggly');
 
 /**
  * Exposes Bunyan Logger to the application
@@ -11,24 +10,9 @@ const Bunyan2Loggly = require('bunyan-loggly');
  * @see {@link https://github.com/trentm/node-bunyan|bunyan}
  */
 
-const bunyanConfig = config.bunyan;
-bunyanConfig.streams = bunyanConfig.streams || [];
-bunyanConfig.streams.push({level: bunyanConfig.level, stream: process.stdout});
-
-if (process.env.NODE_ENV === 'development') {
-  const logglyConfig = {
-    token: 'fc510f5a-38d2-4ebd-8aac-e7f35fee4f93',
-    subdomain: 'stayyolo'
-  };
-  const bufferLength = 5; // sends the logs to loggly on every 5th log
-  const bufferTimeout = 1000; // sends the logs to loggly every 1second even if it doesn't hv 5 logs
-  const logglyStream = new Bunyan2Loggly(logglyConfig, bufferLength, bufferTimeout);
-  bunyanConfig.streams.push({type: 'raw', stream: logglyStream});
-}
-
 /**
  * @type {Logger}
  */
-const logger = bunyan.createLogger(bunyanConfig);
+const logger = bunyan.createLogger(config.bunyan);
 
 module.exports = logger;
