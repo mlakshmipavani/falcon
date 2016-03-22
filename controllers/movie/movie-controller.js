35e2d1f898bb9a654826dc78b3720b408c617276
Jaydeep <jaydp17@gmail.com>
2016-03-22 19:02:53 +0530

    typo fix

--- controllers/movie/movie-controller.js
+++ controllers/movie/movie-controller.js
@@ -15,7 +15,7 @@ class MovieController {
     return GeoIp.getDetails(clientIp)
       .then(details => BotMovieDao.getNearestCity(details.latitude, details.longitude))
       .then((/*BmsCity*/city) => city.SubRegionCode)
-      .then((/*string*/ cityCode) => this._getMovies(langArr, 'BANG'));
+      .then((/*string*/ cityCode) => this._getMovies(langArr, cityCode));
   }
 
   static _getMovies(/*Array<string>*/ langArr, /*string*/ cityCode) {
