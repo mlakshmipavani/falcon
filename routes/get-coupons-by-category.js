407587622d4a8cac2667677092bc14467fea3ce3
lakshmi <pavani.lakshmi233@gmail.com>
2016-07-05 15:09:44 +0000

    Delete get-coupons-by-category.js

--- routes/get-coupons-by-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const request = require('request-promise');
-const couponDunia = require('../controllers/coupons-by-category');
-
-const userInput = 'Travel';
-couponDunia.getCouponsByCategory(userInput);
-
-
